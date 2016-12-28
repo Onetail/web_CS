2b44a19d9c11862136b0877d82d83dc87e1b8377
Spaert <asd850620@gmail.com>
2016-12-28 23:34:17 +0800

    can goto the right page now

--- Login_Web.py
+++ Login_Web.py
@@ -2,7 +2,9 @@ import requests
 import codecs
 from bs4 import BeautifulSoup
 
+
 def login_to_grade_inquiry(studentid, password):
+    
     data = {
         "myurl": "http://portal.tku.edu.tw/aissinfo/emis/tmw0012.aspx",  # 登入成績網站
         "ln": "zh_TW",
@@ -16,8 +18,17 @@ def login_to_grade_inquiry(studentid, password):
     s.post(url="https://sso.tku.edu.tw/NEAI/login2.do?action=EAI", data=data)  # Post_url
     year = str(103)  # 暫設103年
     sem = str(1)  # 暫設第一學期
-    url = "http://sso.tku.edu.tw/aissinfo/emis/TMWS030_result.aspx?yr=" + year + "&sem=" + sem + "&Quar=0"
-    print(url)
-    r = s.get(url)  # 需要顯示的url
+    gradeurl = 'http://sso.tku.edu.tw/aissinfo/emis/TMWS030.aspx'
+    s.get(gradeurl, allow_redirects=False)
+    url = "http://sso.tku.edu.tw/aissinfo/emis/TMWS030_result.aspx" # 進入最終爬蟲頁面
+    data2 = {
+        "yr": year,
+        "sem": sem,
+        "Quar": "0"
+    }
+    r = s.get(url, params=data2, allow_redirects=False)
+    r.encoding = 'utf-8'
+    content = BeautifulSoup(r.text, "lxml")
+    print(content)
     file = codecs.open("grade.html", "w", "utf-8")  # 暫存到grade.html
     file.write(r.text)
